7099cacabd6a43d04edc2be4a4b7cf5639471299
Mike Mambwe <88389755+aeprodigy@users.noreply.github.com>
2023-01-15 00:07:23 -0800

    Update index.js
    Changes to the original Javascript to make the Nvabar stickiness to stay fixed.

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-humburger = document.getElementById(".humburger");
-//The line above deals with the hamburger.
-humburger.onclick= function(){
-    navbar = document.querySelector(".nav-bar");
-    navbar.classList.toggle("active");
-
-}
-
-//THIS LINES BELOW DEALS WITH THE STICKY NAVBAR
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", this.window.scrollY > 0);
 });
 
-//Navbar Toggle
-function toggleMobileMenu(menu){
+//
 
-    menu.classList.toggle('open');
-    
-}
+
+//The line above deals with the hamburger.
+humburger = document.querySelector(".humburger");
+humburger.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
+
+//THIS LINES BELOW DEALS WITH THE STICKY NAVBAR
